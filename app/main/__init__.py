46b33b54cff740786085e7127cfc368686f621e3
Makhabane Christopher Mipi <christopher.mipi@gmail.com>
2018-11-07 20:39:02 +0200

    replacing flask with flask api

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask
 from flask_restful import Api
+from flask_api import FlaskAPI
 
 from app.main.controllers import AppInfo, TableData, DatabasesDetails, Test
 from .config import config_by_name
 
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = FlaskAPI(__name__)
     app.config.from_object(config_by_name[config_name])
 
     api = Api(app)
