93b13397e16553c1fb716e143ea9d72706cfda30
Makhabane Christopher Mipi <christopher.mipi@gmail.com>
2018-11-08 19:34:31 +0200

    some small improvements

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,13 +3,20 @@ from flask_api import FlaskAPI
 
 from app.main.controllers import AppInfo, TableData, DatabasesDetails, Test
 from .config import config_by_name
+from flask import got_request_exception
+
+
+def log_exception(sender, exception, **extra):
+    """ Log an exception to our logging framework """
+    sender.logger.debug(':> Got exception during processing: %s', exception)
 
 
 def create_app(config_name):
     app = FlaskAPI(__name__)
     app.config.from_object(config_by_name[config_name])
 
-    api = Api(app)
+    got_request_exception.connect(log_exception, app)
+    api = Api(app, catch_all_404s=True)
 
     api.add_resource(Test, '/test')
     api.add_resource(AppInfo, '/')
--- app/main/config.py
+++ app/main/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'GRSVwrt5hygh!$$@%RGWCDH%$EEY')
     DEBUG = False
 
 
